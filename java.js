7cff723c0946ed6ef99e96c937166133804b9038
Jessica Petherbridge <jp2049@hw.ac.uk>
2023-09-29 13:02:34 +0100

    Cookie Popup editied
    Edited the cookie popup so less code is needed to do the same thing. Also made the button function

--- java.js
+++ java.js
@@ -2,30 +2,8 @@ function buttonClicked(){
     const button = document.getElementById('button').innerHTML = 'Button Clicked!'
 };
 
-let popUp = document.getElementById("cookiePopup");
-
-document.getElementById("acceptCookie").addEventListener("click", () =>{
-    let d = new Date();
-    d.setMinutes(2 + d.getMinutes());
-    document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
-    popUp.classList.add("hideCookie");
-    popUp.classList.remove("showCookie");
-});
-
-const checkCookie = () => {
-    let input = document.cookie.split("=");
-    if(input[0] == "myCookieName"){
-        popUp.classList.add("hideCookie");
-        popUp.classList.remove("showCookie");
-    }
-    else{
-        popUp.classList.add("showCookie");
-        popUp.classList.add("hideCookie");
-    }
-};
-
-window.onload = () => {
-    setTimeout(() => {
-        checkCookie();
-    }, 2000);
-};
+function cookieClicked(){
+    const cookieButton = document.getElementById('acceptCookie').innerHTML = 'Clicked!'
+    cookiePopup.classList.add("hide");
+    cookiePopup.classList.remove("show");
+}   
