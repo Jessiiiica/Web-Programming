d7fa1677d23219344f40fdb4ade0455a69dfafa2
Jessica Petherbridge <jp2049@hw.ac.uk>
2023-10-11 09:35:31 +0100

    Added a Navigation Bar
    I've added a top navigation bar that changes colours when hovered over, they do not load in a new page yet.

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 function buttonClicked(){
     const button = document.getElementById('button').innerHTML = 'Button Clicked!'
-};
+}
 
 function cookieClicked(){
     document.getElementById("cookiePopup").style.display = 'none';
