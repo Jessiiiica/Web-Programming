3aec33bf16a9396faef8ac1c68ebdac9d6ca3040
Jessica Petherbridge <jp2049@hw.ac.uk>
2023-10-20 16:31:48 +0100

    Working on Contact Form
    Gotten the contact form closer to working however still need to do more for it to be fully functional

--- java.js
+++ java.js
@@ -34,48 +34,39 @@ function showSlides(n) {
 
 }
 
-let saveFile =() => {
+let saveFile = () => {
     const name = document.getElementById("firstName");
     const lastName = document.getElementById("lastName");
     const email = document.getElementById("email");
     const country = document.getElementById("country");
-    const message = document.getElementById("contents");
+    const contents = document.getElementById("contents");
 
-    let data = "\r Name: " + name.value 
-    + " \r\n " + "Age: " + lastName.value 
-    + " \r\n " + "Email: " + email.value 
-    + " \r\n " + "Country: " + country.value 
-    + " \r\n " + "Contents: " + message.value;
-
+    let data = "\r Name: " + name.value + " \r\n " + "Age: " + lastName.value + " \r\n " + "Email: " + email.value + " \r\n " + "Country: " + country.value + " \r\n " + "Message: " + contents.value;
     console.log(data);
-
     const textToBLOB = new Blob([data], { type: "text/plain" });
-            var filename = new Date();
-            var month =new Date(); 
+    var filename = new Date();
+    var month =new Date();
+    month = month.getMonth();
 
-            month = month.getMonth();
+    var day = new Date();
+    var day = day.getUTCDate();
 
-            var day = new Date();
-            var day = day.getUTCDate();
+    var year = new Date();
+    var year = year.getUTCFullYear();
 
-            var year = new Date();
-            var year = year.getUTCFullYear();
+    newdate = year + "/" + month + "/" + day;
+    const sFileName = filename;
 
-            newdate = year + "/" + month + "/" + day;
-            const sFileName = filename;
+    let newLink = document.createElement("a");
+    newLink.download = new Date();
 
-            let newLink = document.createElement("a");
-            newLink.download = new Date();
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    } else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
 
-            if(window.webkitURL != null){
-                newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-            }
-            else{
-                newLink.href = window.URL.createObjectURL(textToBLOB);
-                newLink.style.display = "none";
-
-                document.body.appendChild(newLink);
-            }
-
-            newLink.click();
-}
+    newLink.click();
+};
